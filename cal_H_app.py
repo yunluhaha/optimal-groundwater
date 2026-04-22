298e492685e2d69fd524496fd6086de8615bb109
yunluhaha <161475213+yunluhaha@users.noreply.github.com>
2026-04-22 17:05:42 -0500

    Shorten badge label to 访客数

--- cal_H_app.py
+++ cal_H_app.py
@@ -774,7 +774,7 @@ with main_tab2:
 st.markdown("---")
 st.markdown("""
 <div style="text-align:center; padding:0.6rem 0 1rem; color:#7a6a58; font-size:0.82rem;">
-  <img src="https://visitor-badge.laobi.icu/badge?page_id=yunluhaha.optimal-groundwater&left_color=%232d2a24&right_color=%239abfc8&left_text=%E8%AE%BF%E9%97%AE%E4%BA%BA%E6%95%B0"
-       alt="访问人数" style="height:22px; vertical-align:middle; border-radius:4px;"/>
+  <img src="https://visitor-badge.laobi.icu/badge?page_id=yunluhaha.optimal-groundwater&left_color=%232d2a24&right_color=%239abfc8&left_text=%E8%AE%BF%E5%AE%A2%E6%95%B0"
+       alt="访客数" style="height:22px; vertical-align:middle; border-radius:4px;"/>
 </div>
 """, unsafe_allow_html=True)
